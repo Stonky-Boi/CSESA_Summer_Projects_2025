bd439a8f6196224f21dac7bd98d904300e2289ff
Stonky-Boi <arnav63366@gmail.com>
2025-06-18 15:47:10 +0530

    build files

---
+++ Systems Programming/Project 5/build/CMakeFiles/mips_cli.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/arnav/Desktop/GitHub/CSESA_Summer_Projects_2025/Systems Programming/Project 5/src/cli_interface.cpp" "CMakeFiles/mips_cli.dir/src/cli_interface.cpp.o" "gcc" "CMakeFiles/mips_cli.dir/src/cli_interface.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Systems Programming/Project 5/build/CMakeFiles/mips_cli.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mips_cli.dir/src/cli_interface.cpp.o"
+  "CMakeFiles/mips_cli.dir/src/cli_interface.cpp.o.d"
+  "mips_cli"
+  "mips_cli.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mips_cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
